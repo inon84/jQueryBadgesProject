9ec9b2a9b45df0927de289c3a6afd33d83e98b43
Inon Moshkowitz <inon84@gmail.com>
2016-12-05 18:35:11 +0200

    clear

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,32 +4,7 @@ $(document).ready(function() {
     dataType: 'jsonp',
   })
   success: function(response) {
-    console.log("success");
-    addCourses(response.courses.completed);
-    console.log("response", response.courses.completed);
-    function addCourses(courses){
-      var $badges = $('#badges');
-      courses.forEach(function(course){
-        var $course = $('<div />',{
-          'class': 'course'
-        }).appendTo($badges);
-
-        $('<h3 />',{
-          text: course.title
-        }).appendTo($course);
-
-        $('<img />',{
-          src: course.badge
-        }).appendTo($course);
-
-        $('<a />',{
-          'class': 'btn btn-primary',
-          target: '_blank',
-          href: course.url,
-          text: 'See Course'
-        }).appendTo($course);
-
-      })
+    // bllaaa
     }
   })
   /*
