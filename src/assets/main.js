8cef834d2f47d281c17bbc2afc8ddf0aadc96abf
Inon Moshkowitz <inon84@gmail.com>
2016-12-05 18:29:03 +0200

    Done

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,43 @@
-$(function() {
+$(document).ready(function() {
+  $.ajax({
+    url: 'https://www.codeschool.com/users/Inon84.json',
+    dataType: 'jsonp',
+  })
+  .done(function(response) {
+    console.log("success");
+    addCourses(response.courses.completed);
+    console.log("response", response.courses.completed);
+    function addCourses(courses){
+      var $badges = $('#badges');
+      courses.forEach(function(course){
+        var $course = $('<div />',{
+          'class': 'course'
+        }).appendTo($badges);
 
-  // your code will go here
+        $('<h3 />',{
+          text: course.title
+        }).appendTo($course);
 
+        $('<img />',{
+          src: course.badge
+        }).appendTo($course);
+
+        $('<a />',{
+          'class': 'btn btn-primary',
+          target: '_blank',
+          href: course.url,
+          text: 'See Course'
+        }).appendTo($course);
+
+      })
+    }
+  })
+  /*
+  .fail(function() {
+    console.log("error");
+  })
+  .always(function() {
+    console.log("complete");
+  });
+  */
 });
